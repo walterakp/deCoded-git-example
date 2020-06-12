a30694e6843eb977160c5222ee079a808a3d24e1
Walter <walterg1akp@gmail.com>
2020-06-12 18:51:30 +0100

    added new JS files

---
+++ deCoded/for.js
@@ -0,0 +1,11 @@
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+// for (let i = 0; i < colors.length; i++){
+//     console.log (colors[i]);
+// }
+
+// The better way with FOR OF:
+for (color of colors){
+    console.log (color);
+}
+
